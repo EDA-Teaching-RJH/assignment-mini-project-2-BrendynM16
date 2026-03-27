3b650b8c5f60bd33b95d82ed606dec1794c970e2
Brendyn <bam30@kent.ac.uk>
2026-03-27 10:41:27 +0000

    Add DatingAdviceBot class with CSV loading and NLP training
    Added a new DatingAdviceBot class that extends BaseBot and includes functionality for loading a CSV file and training an NLP engine.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 #Importing my custom library
 from nlp_engine import NLPEngine
 
-# 1. OOP: The Superclass base, which will be the basebot for the algorithm
+# 1.The Superclass base, which will be the basebot for the algorithm
 class BaseBot:
     def __init__(self, bot_name):
         self.bot_name = bot_name
@@ -15,7 +15,22 @@ class BaseBot:
         print("Type 'quit' at any time to exit.\n")
 
     def log_interaction(self, user_input):
-        #Writes the user's input to a log file
+        #Writes the user's input to a log file to document so i can further train the bot
         with open("chat_history.txt", "a") as file:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             file.write(f"[{timestamp}] User Asked: {user_input}\n")
+class DatingAdviceBot(BaseBot):
+    def __init__(self, csv_filename='dating_data.csv'):
+        # Call the superclass constructor
+        super().__init__("Data-Driven Dating Advice Bot")
+        
+        
+        try:
+            self.df = pd.read_csv(csv_filename)
+        except FileNotFoundError:
+            print(f"❌ Error: Could not find '{csv_filename}'.")
+            sys.exit()
+        
+        #training the Custom NLP Library
+        self.nlp = NLPEngine()
+        self.nlp.train(self.df['Situation'])
