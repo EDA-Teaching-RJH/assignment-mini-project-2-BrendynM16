c1fd9f8d8d093d09bce2d886f8918b18349ff105
Brendyn <bam30@kent.ac.uk>
2026-03-27 10:38:55 +0000

    add interaction logging

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import pandas as pd
 import sys
 from datetime import datetime
 
-#Importing my newly created custom library
+#Importing my custom library
 from nlp_engine import NLPEngine
 
-# 1. OOP: The Superclass base which will e the basebot for the algorithm
+# 1. OOP: The Superclass base, which will be the basebot for the algorithm
 class BaseBot:
     def __init__(self, bot_name):
         self.bot_name = bot_name
@@ -14,3 +14,8 @@ class BaseBot:
         print(f"🤖 Welcome to the {self.bot_name}! 🤖")
         print("Type 'quit' at any time to exit.\n")
 
+    def log_interaction(self, user_input):
+        #Writes the user's input to a log file
+        with open("chat_history.txt", "a") as file:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file.write(f"[{timestamp}] User Asked: {user_input}\n")
