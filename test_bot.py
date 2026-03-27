7c331be07f125f392c141a03cb19f5fec8cd47c8
Brendyn <bam30@kent.ac.uk>
2026-03-27 10:31:07 +0000

    Implement setUp method for NLPEngine tests
    Add setUp method to initialize NLPEngine and corpus.

--- test_bot.py
+++ test_bot.py
@@ -2,3 +2,11 @@ import unittest
 from nlp_engine import NLPEngine
 
 class TestNLPEngine(unittest.TestCase):
+    def setUp(self):
+        """will run before every test to set up the correct environment."""
+        self.nlp = NLPEngine()
+        self.corpus = [
+            "They take hours to text back.",
+            "We fight constantly over small things."
+        ]
+        self.nlp.train(self.corpus)
