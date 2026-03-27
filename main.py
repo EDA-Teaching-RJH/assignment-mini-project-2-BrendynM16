384142be8f0b62e791e837867c3e94d5377c04f7
Brendyn <bam30@kent.ac.uk>
2026-03-27 10:25:28 +0000

    Implement BaseBot constructor and greeting method
    Add constructor and greet method to BaseBot class which greets the user

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ from nlp_engine import NLPEngine
 
 # 1. OOP: The Superclass base which will e the basebot for the algorithm
 class BaseBot:
+    def __init__(self, bot_name):
+        self.bot_name = bot_name
+
+    def greet(self):
+        print(f"🤖 Welcome to the {self.bot_name}! 🤖")
+        print("Type 'quit' at any time to exit.\n")
+
